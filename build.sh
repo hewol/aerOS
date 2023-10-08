4b48d9591a573f2693254798a92c2130ec4d9d57
Xanudu <133562760+XanuduBuster@users.noreply.github.com>
2023-10-08 21:58:26 +0530

    improve build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-rerun=true
 clean() {
     echo "Cleaning work directory"
     sudo umount -Rq work || true
@@ -10,60 +9,70 @@ clean() {
 }
 
 enable_services() {
+    create_symlink() {
+        source="$1"
+        target="$2"
+        printf "Creating symlink for %s\n" "$source"
+        ln -svf "$source" "archlive/airootfs/etc/systemd/system/$target"
+    }
 
-    printf "Creating symlinks for Display Manager.\n"
-    ln -svf /usr/lib/systemd/system/graphical.target archlive/airootfs/etc/systemd/system/default.target
-    ln -svf /usr/lib/systemd/system/gdm.service archlive/airootfs/etc/systemd/system/display-manager.service
-    echo
-
-    printf "Creating symlinks for Network Manager.\n"
-    mkdir -p archlive/airootfs/etc/systemd/system/multi-user.target.wants
-    ln -svf /usr/lib/systemd/system/NetworkManager.service archlive/airootfs/etc/systemd/system/multi-user.target.wants/NetworkManager.service
-    ln -svf /usr/lib/systemd/system/NetworkManager-dispatcher.service archlive/airootfs/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
-    mkdir -p archlive/airootfs/etc/systemd/system/network-online.target.wants
-    ln -svf /usr/lib/systemd/system/NetworkManager-wait-online.service archlive/airootfs/etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service
-    echo
+    create_symlink "/usr/lib/systemd/system/graphical.target" "default.target"
+    create_symlink "/usr/lib/systemd/system/gdm.service" "display-manager.service"
+    create_symlink "/usr/lib/systemd/system/NetworkManager.service" "multi-user.target.wants/NetworkManager.service"
+    create_symlink "/usr/lib/systemd/system/NetworkManager-dispatcher.service" "dbus-org.freedesktop.nm-dispatcher.service"
+    create_symlink "/usr/lib/systemd/system/NetworkManager-wait-online.service" "network-online.target.wants/NetworkManager-wait-online.service"
 }
 
-if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]; then
-    echo -n "Chaotic AUR is not installed in your system. Do you want to install it? [Y/n] " && read install
-        if [[ ${install:0:1} == "y" || ${install:0:1} == "" ]]; then
-            echo "Installing chaotic aur"
-            sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-            sudo pacman-key --lsign-key 3056513887B78AEB
-            sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-            if [ -f /etc/pacman.d/chaotic-mirrorlist ]; then
-                echo "Successfully installed chaotic aur ... continuing with the build."
-            else
-                echo "Chaotic aur mirrorlist not found .. aborting build."
-                rerun=false
-            fi
+install_chaotic_aur() {
+    echo "Chaotic AUR is not installed on your system. Do you want to install it? [Y/n] "
+    read -r install
+    if [[ ${install:0:1} == "y" || ${install:0:1} == "" ]]; then
+        echo "Installing chaotic aur"
+        sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+        sudo pacman-key --lsign-key 3056513887B78AEB
+        sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+        if [ -f /etc/pacman.d/chaotic-mirrorlist ]; then
+            echo "Successfully installed chaotic aur ... continuing with the build."
         else
             echo "Chaotic aur mirrorlist not found .. aborting build."
             rerun=false
         fi
+    else
+        echo "Chaotic aur mirrorlist not found .. aborting build."
+        rerun=false
+    fi
+}
+
+if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]; then
+    install_chaotic_aur
 fi
 
+rerun=true
 while $rerun; do
-    test -d work && clean
-    # ! sudo mkarchiso -v archlive 2>&1 | tee debug.log
+    if test -d work; then
+        clean
+    fi
+
     enable_services
-    ! sudo mkarchiso -v archlive
-    retcod=${PIPESTATUS[0]}
-    if [ $retcod == 0 ]; then
-        rerun=false
-    else
+
+    if ! sudo mkarchiso -v archlive; then
         echo
-        # echo "Build failed with the following errors"
-        # grep -i 'error\|warning\|failed' debug.log
-        echo -n "Do you want to retry the build? [y/N] " && read retry
+        echo -n "Build failed. Do you want to retry the build? [y/N] "
+        read -r retry
         if [[ ${retry:0:1} != "y" ]]; then
             rerun=false
         else
             echo
         fi
+    else
+        rerun=false
     fi
 done
 
 test -d work && clean
-[[ $retcod == 0 ]] && echo "Building finished successfully." || echo "Building failed."
+
+if [ "$retcod" -eq 0 ]; then
+    echo "Building finished successfully."
+else
+    echo "Building failed."
+fi
