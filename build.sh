3dccbbdb1a8fbe27f7fd2eab601a2769b29ac4fb
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-12-28 15:14:12 +0100

    screwed it up
    it still removes gdm

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ clean() {
     sudo umount -Rq work || true
     sudo rm -r work
 
-	if pacman -Qg gnome > /dev/null ; then
+    if pacman -Qg gnome > /dev/null ; then
+        echo "Not removing build packages because GNOME is installed."
+    else
         echo "Cleaning required build packages"
         sudo pacman -Rns gdm networkmanager network-manager-applet --noconfirm
-    else
-        echo "Not removing build packages because GNOME is installed."
+        
     fi
 }
 
